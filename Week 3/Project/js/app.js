4da71813fd43a9974a9f0364b1a02325534ab4f3
Noor Ali <nooralib180@gmail.com>
2024-07-06 10:06:53 +0500

    commenting the stuffs

--- Week 3/Project/js/app.js
+++ Week 3/Project/js/app.js
@@ -12,41 +12,57 @@ const specialCharEl = document.querySelector("#special-characters");
 
 /////////////////////
 
+// function to render the updates in UI...
 const render = function () {
+  // input from user...
   const input = inputEl.value;
 
+  // CHARACTERS (everything which moves the cursor forward is a character)
   const charactersCount = calcCharactersCount(input);
   charEl.innerHTML = charactersCount;
 
+  // WORDS
   const wordsCount = calcWordsCount(input);
   wordEl.innerHTML = wordsCount;
 
+  // SENTENCES (ends with punctuation mark [. ? :])
   const sentencesCount = calcSentencesCount(input);
   sentEl.innerHTML = sentencesCount;
 
+  // PARAGRAPHS (starting new paragraph after two line breaks [ \n ])
   const paragraphsCount = calcParagraphsCount(input);
   paraEl.innerHTML = paragraphsCount;
 
+  // SPACES (white spaces in the text)
   const spacesCount = calcSpacesCount(input);
   spaceEl.innerHTML = spacesCount;
 
+  // LETTERS (english alphabets a-z A-Z)
   const lettersCount = calcLettersCount(input);
   letterEl.innerHTML = lettersCount;
 
+  // DIGITS (digits from 0 - 9)
   const digitsCount = calcDigitsCount(input);
   digitEl.innerHTML = digitsCount;
 
+  // SPECIAL CHARACTERS (. , ! ^ & * ~ # ETC...)
   const specialCharactersCount = calcSpecialCharactersCount(input);
   specialCharEl.innerHTML = specialCharactersCount;
-
 };
 
+// listening event whenever a key is pressed and lifted up...
+inputEl.addEventListener("keyup", render);
+
+/////////////////////////////////////////////////
+
+// function to count characters in the input...
 const calcCharactersCount = function (str) {
   const output = str.length;
 
   return output === 0 ? "-" : output;
 };
 
+// function to count total words present in the input...
 const calcWordsCount = function (str) {
   const output = str
     // replacing all the punctuation marks with "NaW", later to filter out.
@@ -58,6 +74,7 @@ const calcWordsCount = function (str) {
   return output === 0 ? "-" : output;
 };
 
+// function to count total sentences present in the input...
 const calcSentencesCount = function (str) {
   const output = str
     .split(/[.?:]/)
@@ -69,6 +86,7 @@ const calcSentencesCount = function (str) {
   return output === 0 ? "-" : output;
 };
 
+// function to count total paragraphs present in the input...
 const calcParagraphsCount = function (str) {
   const output = str
     .split("\n\n")
@@ -77,30 +95,33 @@ const calcParagraphsCount = function (str) {
   return output === 0 ? "-" : output;
 };
 
+// functionto count total spaces present in the input...
 const calcSpacesCount = function (str) {
   const output = str.split(" ").length - 1;
 
   return output === 0 ? "-" : output;
 };
 
+// function to count total letters present in the input...
 const calcLettersCount = function (str) {
   const output = str.split(/[a-zA-Z]/g).length - 1;
 
   return output === 0 ? "-" : output;
 };
 
+// function to count total digits present in the input...
 const calcDigitsCount = function (str) {
   const output = str.split(/[0-9]/g).length - 1;
 
   return output === 0 ? "-" : output;
 };
 
-const calcSpecialCharactersCount = function(str) {
-     const output =
-       str.split(/[!@#\$%\^\&\*\(\)\-\_\=\+\[\{\]\}\\|;:'",<\.>\/\?]/g).length -
-       1;
+// function to calculate total special characters present in the input...
+const calcSpecialCharactersCount = function (str) {
+  const output =
+    str.split(/[!@#\$%\^\&\*\(\)\-\_\=\+\[\{\]\}\\|;:'",<\.>\/\?]/g).length - 1;
 
-     return output === 0 ? "-" : output;
-}
+  return output === 0 ? "-" : output;
+};
 
-inputEl.addEventListener("keyup", render);
+////////////////////////////////////////////////////////////////////
